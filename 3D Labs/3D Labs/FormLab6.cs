f38055ec56a4ef334f4d22139d17bea1c1a1a173
SskytT <regmail21@yandex.ru>
2024-11-04 22:01:58 +0300

    Task 7 Surface

--- 3D Labs/3D Labs/FormLab6.cs
+++ 3D Labs/3D Labs/FormLab6.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
 using _3D_Labs.Graphic;
 using _3D_Labs.Graphic.Objects;
 using System.Diagnostics;
-
+using System.Net.Mail;
 
 namespace _3D_Labs
 {
@@ -25,42 +25,99 @@ namespace _3D_Labs
         Axis cameraMoveAxis;
         float cameraMoveLength;
         bool isMoving;
+        List<Func<float, float, float>> funcs;
 
         public FormLab6()
         {
             InitializeComponent();
             timer = new System.Windows.Forms.Timer();
-            timer.Interval = 1; // Интервал в миллисекундах (например, 100 ms)
+            timer.Interval = 1; 
             timer.Tick += TimerTickCameraMove;
 
             cameraMoveAxis = Axis.X;
             isMoving = false;
+            funcs = new List<Func<float, float, float>>();
 
             bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             this.scene = new Scene();
 
             Polyhedron tetrahedron = SimplePolyhedrons.Tetrahedron(200);
-            Polyhedron tetrahedron1 = tetrahedron.DeepCopy();
-            Polyhedron tetrahedron2 = tetrahedron.DeepCopy();
 
-            //AffineTransform.RotationAroundZ(tetrahedron.points, 45);
-            AffineTransform.Offset(tetrahedron.points, -200, 0, 1500);
-            AffineTransform.Offset(tetrahedron1.points, 200, 0, 1500);
-            AffineTransform.Offset(tetrahedron2.points, 0, 0, 1800);
+            AffineTransform.Offset(tetrahedron.points, 0, 0, 1500);
             scene.polyhedrons.Add(tetrahedron);
-            scene.polyhedrons.Add(tetrahedron1);
-            scene.polyhedrons.Add(tetrahedron2);
+
             for (int i = 0; i < scene.polyhedrons.Count; i++)
             {
                 comboBox1.Items.Add(i);
             }
-            //AffineTransform.Offset(scene.polyhedrons[0].points, 0, 100, -500);
-            //for (int i = 0; i < 10000; i++)
-            //    scene.polyhedrons.Add(tetrahedron1.DeepCopy());
 
-            scene.camera.target.X = 0;
+            funcs.Add((x, y) => x * x - y * y);
+            funcs.Add((x, y) => 2 * x + 3 * y + 5);
+            funcs.Add((x, y) => x * x + y * y);
+            funcs.Add((x, y) => -(x * x) - y * y);
+            funcs.Add((x, y) => 1 / (5 * (x * x + y * y) + 1));
+            funcs.Add((x, y) => 10* (MathF.Sin(x) + MathF.Sin(y)));
+            funcs.Add((x, y) => MathF.Sqrt(x*x + y*y));
+            funcs.Add((x, y) => 6*MathF.Exp(-(x*x+y*y)/2*16));
+
+            comboBox2.Items.Add("Седло");
+            comboBox2.Items.Add("Плоскость");
+            comboBox2.Items.Add("Эллиптический параболоид вверх");
+            comboBox2.Items.Add("Эллиптический параболоид вниз");
+            comboBox2.Items.Add("Гиперболическая поверхность");
+            comboBox2.Items.Add("Волновая поверхность (синусоидальная)");
+            comboBox2.Items.Add("Конус");
+            comboBox2.Items.Add(" Гауссова поверхность");
+
+
+            scene.polyhedrons.Add(Surface.Create(-1, -1, 1, 1, 15, 15, funcs[0]));
+            AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 100, 100, 100);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 300, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-100, -100, 100, 100, 10, 10, funcs[1]));
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 600, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-1, -1, 1, 1, 15, 15, funcs[2]));
+            AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 100, 100, 100);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 900, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-1, -1, 1, 1, 15, 15, funcs[3]));
+            AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 100, 100, 100);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 1200, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-1, -1, 1, 1, 15, 15, funcs[4]));
+            AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 270);
+            AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 100, 100, 100);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 1500, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-100, -100, 100, 100, 30, 30, funcs[5]));
+            AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 90);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 1800, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-100, -100, 100, 100, 10, 10, funcs[6]));
+            AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 90);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 2100, 0, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.polyhedrons.Add(Surface.Create(-1, -1, 1, 1, 20, 20, funcs[7]));
+            AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 270);
+            AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 100, 100, 100);
+            AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 2400, -100, 1500);
+            comboBox1.Items.Add(scene.polyhedrons.Count - 1);
+
+            scene.camera.position.X = 1100;
+            scene.camera.position.Y = 0;
+            scene.camera.position.Z = -800;
+
+            scene.camera.target.X = 1100;
             scene.camera.target.Y = 0;
-            scene.camera.target.Z = 10000;
+            scene.camera.target.Z = 9200;
 
             DrawScene();
         }
@@ -74,13 +131,18 @@ namespace _3D_Labs
 
             for (int j = 0; j < edges.Count; j++)
             {
+                
                 var x = new PointF(edges[j].FirstPoint.X, edges[j].FirstPoint.Y);
                 var xx = new PointF(edges[j].SecondPoint.X, edges[j].SecondPoint.Y);
                 if ((edges[j].FirstPoint.Z > 0 && edges[j].SecondPoint.Z > 0 && scene.projectionNumber == 0) || scene.projectionNumber == 1)
                     g.DrawLine(Pens.Black, x, xx);
+                
                 //richTextBox1 += $"{x.X} {x.Y} | {xx.X} {xx.Y}\r\n";
             }
             pictureBox1.Image = bitmap;
+
+            CameraPosText.Text = $"X: {scene.camera.position.X}  Y: {scene.camera.position.Y}  Z: {scene.camera.position.Z}";
+            CameraTargetText.Text = $"X: {scene.camera.target.X}  Y: {scene.camera.target.Y}  Z: {scene.camera.target.Z}";
         }
 
         private void TimerTickCameraMove(object sender, EventArgs e)
@@ -98,7 +160,7 @@ namespace _3D_Labs
             if (e.Button == MouseButtons.Left)
             {
                 isMoving = true;
-                float speed = 1;
+                float speed = 10;
                 if (textBox1.Text.Length > 0)
                     float.TryParse(textBox1.Text, out speed);
                 switch(clickedButton.Name)
@@ -279,5 +341,82 @@ namespace _3D_Labs
             scene.projectionNumber = 1;
             DrawScene();
         }
+
+        private void CreateSurface(object sender, EventArgs e)
+        {
+            float x1 = 0;
+            float y1 = 0;
+            float x2 = 0;
+            float y2 = 0;
+
+            if (SurfaceX1.Text.Length > 0)
+                float.TryParse(SurfaceX1.Text, out x1);
+
+            if (SurfaceY1.Text.Length > 0)
+                float.TryParse(SurfaceY1.Text, out y1);
+
+            if (SurfaceX2.Text.Length > 0)
+                float.TryParse(SurfaceX2.Text, out x2);
+
+            if (SurfaceY2.Text.Length > 0)
+                float.TryParse(SurfaceY2.Text, out y2);
+
+            int partitionsCountX = 1;
+            int partitionsCountY = 1;
+
+            if (SurfaceCountX.Text.Length > 0)
+                int.TryParse(SurfaceCountX.Text, out partitionsCountX);
+
+            if (SurfaceCountY.Text.Length > 0)
+                int.TryParse(SurfaceCountY.Text, out partitionsCountY);
+
+            if (partitionsCountX < 1)
+                partitionsCountX = 1;
+
+            if (partitionsCountY < 1)
+                partitionsCountY = 1;
+
+            int funcNumber = comboBox2.SelectedIndex;
+
+            if (funcNumber != -1)
+            {
+                scene.polyhedrons.Add(Surface.Create(x1, y1, x2, y2, partitionsCountX, partitionsCountY, funcs[funcNumber]));
+                comboBox1.Items.Add(scene.polyhedrons.Count-1);
+                if (funcNumber == 0 || funcNumber == 2 || funcNumber == 3)
+                {
+                    AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 10, 10, 10);
+                    AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, scene.camera.position.X, scene.camera.position.Y, scene.camera.position.Z + 100);
+                }
+                else if (funcNumber == 1)
+                {
+                    AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, scene.camera.position.X, scene.camera.position.Y, scene.camera.position.Z + 500);
+                }
+                else if (funcNumber == 4)
+                {
+                    AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 10, 10, 10);
+                    AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 270);
+                    AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, scene.camera.position.X, scene.camera.position.Y-5, scene.camera.position.Z + 40);
+                }
+                else if (funcNumber == 6)
+                {
+                    AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 90);
+                    AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, scene.camera.position.X, scene.camera.position.Y, scene.camera.position.Z + 100);
+                }
+                else if (funcNumber == 7)
+                {
+                    AffineTransform.ScallingRelativeCenter(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 10, 10, 10);
+                    AffineTransform.RotationAroundX(scene.polyhedrons[scene.polyhedrons.Count - 1].points, 270);
+                    AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, scene.camera.position.X, scene.camera.position.Y- 25, scene.camera.position.Z + 100);
+
+                }
+                else
+                {
+                    AffineTransform.Offset(scene.polyhedrons[scene.polyhedrons.Count - 1].points, scene.camera.position.X, scene.camera.position.Y, scene.camera.position.Z + 100);
+                }
+                DrawScene();
+            }
+        }
+
+        
     }
 }
---
+++ 3D Labs/3D Labs/Graphic/Surface.cs
@@ -0,0 +1,60 @@
+﻿using _3D_Labs.Graphic.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Labs.Graphic
+{
+    public class Surface
+    {
+        public static Polyhedron Create(float x1, float y1, float x2, float y2, int partitionsCountX, int partitionsCountY, Func<float, float, float> func)
+        {
+            float startX = Math.Min(x1, x2);
+            float finishX = Math.Max(x1, x2);
+
+            float startY = Math.Min(y1, y2);
+            float finishY = Math.Max(y1, y2);
+
+            float stepX = (finishX - startX) / partitionsCountX;
+            float stepY = (finishY - startY) / partitionsCountY;
+
+            float currentX = startX;
+            float currentY = startY;
+
+            List<Objects.Point> points = new List<Objects.Point>();
+
+
+            for (int i = 0; i <= partitionsCountY; i++)
+            {
+                for (int j = 0; j <= partitionsCountX; j++)
+                {
+                    points.Add(new Objects.Point(currentX, currentY, func(currentX, currentY)));
+                    currentX += stepX;
+                }
+                currentX = startX;
+                currentY += stepY;
+            }
+
+            List<Face> faces = new List<Face>();
+
+            for (int i = 0; i < partitionsCountY; i++)
+            {
+                for (int j = 0; j < partitionsCountX; j++)
+                {
+                    faces.Add(new Face(new List<Objects.Point> { points[i * (partitionsCountX+1)+j],
+                                                                 points[(i + 1) * (partitionsCountX+1)+j],
+                                                                 points[i * (partitionsCountX+1) + j + 1 ] }));
+
+                    faces.Add(new Face(new List<Objects.Point> { points[(i + 1) * (partitionsCountX+1) + j],
+                                                                 points[(i + 1) * (partitionsCountX+1) + j + 1],
+                                                                 points[i * (partitionsCountX+1) + j + 1 ] }));
+                }
+            }
+
+            return new Polyhedron(faces);
+
+        }
+    }
+}
