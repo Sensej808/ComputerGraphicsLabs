96292c4b2cf363e74fe00de2cc29fce3b5cf6756
nathanbaber <92463132+nathanbaber@users.noreply.github.com>
2024-11-12 21:10:32 +0300

    Add files via upload
    Add task2

---
+++ Lab5/Lab5/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LAB5G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(287, 382);
+            this.trackBar2.Maximum = 200;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(222, 56);
+            this.trackBar2.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 382);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(222, 56);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество итераций";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(294, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Шероховатость";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.trackBar2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab5/Lab5/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LAB5G
+{
+    public partial class Form1 : Form
+    {
+        private int width = 800;
+        private int height = 600;
+        private int iterations;
+        private float roughness;
+        private List<PointF> points;
+        private int currentStep;
+        private Timer timer; // Таймер для автоматического воспроизведения
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; // Для предотвращения мерцаний
+            timer = new Timer();
+            timer.Interval = 1000; // Время между шагами в миллисекундах (1 секунда)
+            timer.Tick += (sender, e) => StepMidpointDisplacement(); // Автоматический шаг при каждом тикере таймера
+            InitializeParameters();
+        }
+        private void InitializeParameters()
+        {
+            GenerateInitialPoints();
+            iterations = trackBar1.Value;
+            roughness = trackBar2.Value;
+            currentStep = 0; // Сбросить текущий шаг
+        }
+
+        private void GenerateInitialPoints()
+        {
+            points = new List<PointF>
+            {
+                new PointF(0, height / 4),
+                new PointF(width, height / 4)
+            };
+        }
+
+        private void StepMidpointDisplacement()
+        {
+            if (currentStep < iterations)
+            {
+                Random random = new Random();
+                List<PointF> newPoints = new List<PointF>();
+                float displacement = roughness / (1 << currentStep); // Размещение уменьшается с каждой итерацией
+
+                for (int j = 0; j < points.Count - 1; j++)
+                {
+                    PointF p1 = points[j];
+                    PointF p2 = points[j + 1];
+                    newPoints.Add(p1);
+
+                    // Находим середину
+                    PointF midPoint = new PointF((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
+                    midPoint.Y += (float)(random.NextDouble() * 2 * displacement - displacement); // Случайное смещение
+                    newPoints.Add(midPoint);
+                }
+
+                newPoints.Add(points[points.Count - 1]); // Добавляем последнюю точку
+                points = newPoints;
+                currentStep++;
+                this.Invalidate(); // Перерисовываем форму после каждого шага
+            }
+            else
+            {
+                timer.Stop(); // Останавливаем таймер, если достигнуто максимальное количество итераций
+                button1.Text = "Старт";
+                MessageBox.Show("Максимальное количество итераций достигнуто.");
+            }
+        }
+
+        private void ToggleAutoPlay()
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop(); // Остановить таймер
+                button1.Text = "Старт"; // Изменить текст кнопки
+            }
+            else
+            {
+                currentStep = 0; // Сбросить номера шагов
+                InitializeParameters(); // Сброс и повторная инициализация параметров
+                timer.Start(); // Запустить таймер
+                button1.Text = "Стоп"; // Изменить текст кнопки
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (points != null && points.Count > 0)
+            {
+                using (Pen pen = new Pen(Color.Blue, 2))
+                {
+                    e.Graphics.DrawLines(pen, points.ToArray());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToggleAutoPlay();
+        }
+    }
+}
