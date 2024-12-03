6c84e7f86a31939c004c57f21081a46eb61fbfa0
Sensej808 <sasha-tkachenko-03@mail.ru>
2024-12-04 02:44:49 +0300

    lab10
    triangle

--- 3D Labs/3D Labs/obj/Debug/net6.0-windows/3D Labs.AssemblyInfo.cs
+++ 3D Labs/3D Labs/obj/Debug/net6.0-windows/3D Labs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3D Labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246098d47ed90d580da332fe060e3194c1cfb6ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("3D Labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3D Labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
